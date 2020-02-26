f3b1f438fb404909095623aa4216df0738072b1e
Vyraal1 <kevin.afable@gmail.com>
2020-02-26 14:48:45 -0500

    chore: adding imports to BSTs directory

---
+++ binary_search_tree/dll_stack.py
@@ -0,0 +1,18 @@
+from doubly_linked_list import DoublyLinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        # a lot of insertion at the head and then
+        self.storage = DoublyLinkedList()
+
+    def push(self, value):
+        return self.storage.add_to_tail(value)
+
+    def pop(self):
+        return self.storage.remove_from_tail()
+
+    def len(self):
+        return self.storage.length
