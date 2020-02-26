fa74bfa3e5b1f88a65078fe1f946ad2d91fee10a
Vyraal1 <kevin.afable@gmail.com>
2020-02-26 01:33:39 -0500

    feat: refacatoring lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,4 +1,5 @@
 from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import ListNode
 
 
 class LRUCache:
@@ -6,34 +7,29 @@ class LRUCache:
 
     def __init__(self, limit=10):
         self.limit = limit
-        self.list = DoublyLinkedList()
+        self.cache = DoublyLinkedList()
         self.storage = {}
-        self.size = self.list.length
+        self.size = self.cache.length
 
     def get(self, key):
         """Retrieves the value associated with the given key. Also needs to move the key-value pair to the end of the order such that the pair is considered most-recently used. Returns the value associated with the key or None if the key-value pair doesn't exist in the cache."""
         if key in self.storage:
-            current = self.list.head
-            while not key == current.value[0]:
-                current = current.next
-            self.list.move_to_end(current)
-            return self.storage[key]
+            self.cache.move_to_end(self.storage[key])
+            return self.storage[key].value[1]
         else:
             return None
 
     def set(self, key, value):
         """Adds the given key-value pair to the cache. The newly- added pair should be considered the most-recently used entry in the cache. If the cache is already at max capacity before this entry is added, then the oldest entry in the cache needs to be removed to make room. Additionally, in the case that the key already exists in the cache, we simply want to overwrite the old value associated with the key with the newly-specified value. """
-        if key in self.storage:  # if key exists, just overwrite, and move to end
-            current = self.list.head
-            while not key == current.value[0]:
-                current = current.next
-            self.storage[key] = value
-            self.list.move_to_end(current)
+        if key in self.storage:  # if key exists, just overwrite tuple, and move to end
+            self.storage[key].value = (key, value)
+            self.cache.move_to_end(self.storage[key])
         else:   # otherwise we care about the size
             if self.size == self.limit:   # make room by removing old
-                self.storage.pop(self.list.head.value[0])
-                self.list.remove_from_head()    # no need to adjust size, since new gets added
+                self.storage.pop(self.cache.head.value[0])
+                self.cache.remove_from_head()    # no need to adjust size, since new gets added
             else:
                 self.size += 1
-            self.storage[key] = value
-            self.list.add_to_tail((key, value))
+            # create a new node?
+            self.cache.add_to_tail((key, value))
+            self.storage[key] = self.cache.tail
