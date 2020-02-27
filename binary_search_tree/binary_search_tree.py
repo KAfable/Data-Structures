1de61e899f2462e8ac04cc2b0def6158da26f642
Vyraal1 <kevin.afable@gmail.com>
2020-02-26 23:19:51 -0500

    feat: repr for BST and iterative contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -10,6 +10,9 @@ class BinarySearchTree:
         self.left = None
         self.right = None
 
+    def __repr__(self):
+        return f"BinaryTree({self.value}, {self.left}, {self.right})"
+
     # Insert the given value into the tree
     def insert(self, value):
         if value < self.value:  # if value is smaller than node
@@ -26,14 +29,25 @@ class BinarySearchTree:
                 # if no node, make a new leaf with value
                 self.right = BinarySearchTree(value)
 
-    def contains(self, target):
+    def contains_recursively(self, target):
         if self.value == target:
             return True
         else:
             if self.value < target:
-                return False if self.right is None else self.right.contains(target)
+                return False if self.right is None else self.right.contains_recursively(target)
             else:
-                return False if self.left is None else self.right.contains(target)
+                return False if self.left is None else self.right.contains_recursively(target)
+
+    def contains_iteratively(self, target):
+        current = self
+        while current:
+            if current.value == target:
+                return True
+            elif current.value < target:
+                current = current.right
+            else:
+                current = current.left
+        return False
 
     def get_max_iterative(self):
         current = self
@@ -50,42 +64,40 @@ class BinarySearchTree:
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
-    # def for_each(self, cb):
-    #     cb(self.value)
-    #     if self.left is not None:
-    #         self.left.for_each(cb)
-    #         if self.right is not None:
-    #             self.right.for_each(cb)
-    #     elif self.right is not None:
-    #         self.right.for_each(cb)
-    #         if self.left is not None:
-    #             self.left.for_each(cb)
-    #     else:
-    #         return
-
     def for_each(self, cb):
         cb(self.value)
         if self.left:
             self.left.for_each(cb)
             if self.right:
                 self.right.for_each(cb)
-        elif self.right:
+        if self.right:
             self.right.for_each(cb)
             if self.left:
                 self.left.for_each(cb)
-        else:
-            return
 
-        # cb(self.value)
-        # if self.left is None and self.right is None:
-        #     return
-        # elif self.left is None and self.right is not None:
-        #     self.right.for_each(cb)
-        # elif self.left is not None and self.right is None:
-        #     self.left.for_each(cb)
-        # else:
-        #     self.left.for_each(cb)
-        #     self.right.for_each(cb)
+    # def for_each(self, cb):
+    #     cb(self.value)
+    #     current = self
+    #     while current.left is not None:
+    #         current = current.left
+    #         cb(current.value)
+    #         print(f'value {current.value}')
+    #         while current.right is not None:
+    #             current = current.right
+    #             cb(current.value)
+    #             print(f'value {current.value}')
+    #     current = self
+    #     while current.right is not None:
+    #         current = current.right
+    #         cb(current.value)
+    #         print(f'value {current.value}')
+    #         while current.left is not None:
+    #             current = current.left
+    #             cb(current.value)
+    #             print(f'value {current.value}')
+
+    # while right exists, go right
+    # while left exists, go left
 
     # DAY 2 Project -----------------------
 
